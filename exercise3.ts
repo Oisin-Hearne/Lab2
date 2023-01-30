4bde507d0352d81168bb531345b338ce262bab7a
Oisin Hearne <oisinhearne@gmail.com>
2023-01-30 09:47:36 +0000

    initial

--- exercise3.ts
+++ exercise3.ts
@@ -1,7 +1,7 @@
 function addition(value: string) {
     console.log("Value is: " + value);
     }
-    
+
     let firstVal: number = 42;
     let secondVal: number = 1;
     let sumOfVals: string = (firstVal + secondVal).toLocaleString();
---
+++ exercise4.ts
@@ -0,0 +1,26 @@
+//Types
+
+//bool
+let myBool: boolean = true;
+
+//number
+let myNum: number = 42;
+
+//strings
+let myString: string = "Hello World";
+
+//arrays
+let myArrayofNums: number[] = [1, 2, 3];
+
+//tuple (array with a defined size w/ defined types)
+let myTuple: [string, number] = [myString, myNum];
+
+//objects
+let myObject: object;
+
+
+console.log("myBool: "+myBool);
+console.log("myNum: "+myNum);
+console.log("myString: "+myString);
+console.log("myArrayofNums: "+myArrayofNums[0] +", "+ myArrayofNums[1] +", "+ myArrayofNums[2]);
+console.log("myTuple: " + myTuple[0] +", "+ myTuple[1])
