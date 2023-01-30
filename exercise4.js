90ac2d06e809f1110b181ca8ca1519dd1da059ee
Oisin Hearne <oisinhearne@gmail.com>
2023-01-30 10:38:01 +0000

    exercise4

---
+++ exercise4.js
@@ -0,0 +1,18 @@
+//Types
+//bool
+var myBool = true;
+//number
+var myNum = 42;
+//strings
+var myString = "Hello World";
+//arrays
+var myArrayofNums = [1, 2, 3];
+//tuple (array with a defined size w/ defined types)
+var myTuple = [myString, myNum];
+//objects
+var myObject;
+console.log("myBool: " + myBool);
+console.log("myNum: " + myNum);
+console.log("myString: " + myString);
+console.log("myArrayofNums: " + myArrayofNums[0] + ", " + myArrayofNums[1] + ", " + myArrayofNums[2]);
+console.log("myTuple: " + myTuple[0] + myTuple[1]);
