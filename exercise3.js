a0b38ef78c9f387c7766c638d45a3ab26cd1fba5
Oisin Hearne <oisinhearne@gmail.com>
2023-01-30 09:32:21 +0000

    Lab 2 30/1/23

---
+++ exercise3.js
@@ -0,0 +1,7 @@
+function addition(value) {
+    console.log("Value is: " + value);
+}
+var firstVal = 42;
+var secondVal = 1;
+var sumOfVals = (firstVal + secondVal).toLocaleString();
+addition(sumOfVals);
---
+++ exercise3.ts
@@ -0,0 +1,8 @@
+function addition(value: string) {
+    console.log("Value is: " + value);
+    }
+    
+    let firstVal: number = 42;
+    let secondVal: number = 1;
+    let sumOfVals: string = (firstVal + secondVal).toLocaleString();
+    addition(sumOfVals);
